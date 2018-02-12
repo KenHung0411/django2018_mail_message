37cb974a235deeb08a27fead37d4860ddf56fd54
unknown <016713@senao.com.tw>
2018-02-12 17:13:27 +0800

    bulid up the base: urls, views, .html ....

--- django_mailService/mailServiceApp/views.py
+++ django_mailService/mailServiceApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+	posts = {"hello":"helloworld"
+	
+	}
+	return render(request,'mailServiceApp/index.html',posts)
